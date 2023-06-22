6c706cf5047e79ff0dbee9a02d459eae10cf759c
YuriyGl86 <yuriygl86@gmail.com>
2023-06-22 23:42:03 +0300

    for_in

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,37 @@
 // TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
+function makeArray(obj) {
+  const arr = [];
+  for (const prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      const propObj = {
+        key: prop,
+        value: obj[prop],
+      };
+      arr.push(propObj);
+    }
+  }
+  return arr;
+}
 
-console.log(sum([1, 2]));
+function sortArray(arr, sortKeys) {
+  return arr.sort((a, b) => {
+    const aIndx = sortKeys.indexOf(a.key);
+    const bIndx = sortKeys.indexOf(b.key);
+
+    if (aIndx !== -1 && bIndx !== -1) {
+      return aIndx - bIndx;
+    }
+    if (aIndx !== -1) {
+      return -1;
+    }
+    if (bIndx !== -1) {
+      return 1;
+    }
+    return a.key.localeCompare(b.key);
+  });
+}
+
+export default function orderByProps(obj, sortKeys) {
+  return sortArray(makeArray(obj), sortKeys);
+}
